12f5f1d2ee1fdba602a3cc1f74ed712f5358d96d
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2024-05-24 21:53:55 -0400

    Read–eval–print loop

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,20 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
 
-    input = input.trim().into();
-    match input {
-        _ => {
-            println!("{input}: command not found");
+        input = input.trim().into();
+        match input {
+            _ => {
+                println!("{input}: command not found");
+            }
         }
     }
 }
